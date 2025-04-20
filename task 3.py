3b1196bf7126b413dfa8f846f9d1541c4cff9b67
Adam864864 <adamabusalama@gmail.com>
2025-04-20 19:49:06 +0200

    Update task 3.py

--- task 3.py
+++ task 3.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
-
-
+st.title('football quiz')
 
 questions = ('who won the ballon dor in 2003:',
              'who is the most player scored goals in the world:',
@@ -21,27 +20,40 @@ options = (('A.Ronaldinho','B.Rivaldo','C.Nedved'),
            ('A.Platini','B.de stefano','C.vanpasten'),
            ('A.USA','B.Uruguay','C.Brasil'))
 
-answers = ('C','C','B','C','c','a','b','b')
-guesses = []
-score = 0
-question_num = 0
 
-for question in questions :
-    print('----------------')
-    print(question)
-    for option in options[question_num]:
-        print(option)
+answers = ('C','C','C','C','B','A','B','B')
+if 'score' not in st.session_state:
+    st.session_state.score = 0
+    st.session_state.q_index = 0
+    st.session_state.guesses = []
 
-    guess = input('enter (A,B,C):').upper()
-    guesses.append(guess)
-    if guess == answers[question_num]:
-        score += 1
-        print('correct')
-    else:
-        print('incorrect')
-        print(f'{answers[question_num]} is the correct answer')
+if session_state.q_index < len(questions):
+    q = questions[st.session_state.q_index]
+    opts = options[st.session_state.q_index]
+    st.subheader(f'Q{st.session_state.q_index +1}:{q}')
+    user_answers = st.radio('choose your answer:',opts)
+
+    if st.button('submit answer'):
+        guess_letter = user_answers[0].upper()
+        st.session_state.guesses.append(guess_letter)
+
+        if guess_letter == answers[st.session_state.q_index]:
+            st.success('correct')
+            st.session_state.score += 1
+        else:
+            st.error(f"❌ Incorrect! The correct answer was: {answers[st.session_state.q_index]}")
+        st.session_state.q_index +=1
+        st.rerun()
+else:
+    st.success(f"🎉 Quiz Completed! Your score: {st.session_state.score}/{len(questions)}")
+
+    if st.button("Restart Quiz"):
+        st.session_state.score = 0
+        st.session_state.q_index = 0
+        st.session_state.guesses = []
+        st.rerun()
 
 
-    question_num += 1
+
 
 
